2109546394a8b7124b28afbb985fb75fa580e4c8
justtryingthingsout <31097061+justtryingthingsout@users.noreply.github.com>
2022-04-30 00:31:30 +0800

    cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,37 @@
 mod patchfinder;
 extern crate memchr;
 
-use std::fs;
+use std::{fs, str};
 use memchr::memmem;
 use patchfinder::*;
 
 fn iboot_ver(buf: &Vec<u8>) -> (usize, bool) {
-    let iboot_ver = match std::str::from_utf8(&buf[0x280..0x2A0]) {
-        Ok(ver) => ver,
-        Err(e) => panic!("Unable to get UTF-8 string!\nerr: {}", e),
-    };
+    let iboot_ver = str::from_utf8(&buf[0x280..0x2A0]).unwrap_or_else(|e| panic!("Unable to get UTF-8 string, err: {}", e));
+    let iboot_type = &buf[0x200..0x204];
 
-    let iboot_type = match std::str::from_utf8(&buf[0x200..=0x204]) {
-        Ok(ver) => ver,
-        Err(e) => panic!("Unable to get UTF-8 string!\nerr: {}", e),
-    };
-
-    if iboot_ver.contains("iBoot") {
-        println!("inputted: {}", iboot_ver);
-        let dot: usize = iboot_ver.find('.').unwrap(); //find index of dot, e.g. iBoot-2817., in order to get version
-        let iboot_ver = iboot_ver[6..dot].parse::<usize>().unwrap();
-        if iboot_type.contains("iBSS") || iboot_type.contains("LLB ") {
-            return (iboot_ver, true);
-        } else {
-            return (iboot_ver, false);
-        }
-    } else {
-        panic!("Invalid image. Make sure image is extracted, iPatcher doesn't support IM4P/IMG4");
-    }
+    if !iboot_ver.contains("iBoot") { panic!("Invalid image. Make sure the image is extracted, iPatcher doesn't support IM4P/IMG4"); }
+    println!("Found: {}", iboot_ver);
+    let dot = iboot_ver.find('.').unwrap(); //find index of dot, e.g. iBoot-2817., in order to get version, should not fail
+    (iboot_ver[6..dot].parse::<usize>().unwrap(), iboot_type == b"iBSS" || iboot_type == b"LLB ")
 }
 
 fn get_rsa_patch(buf: &mut Vec<u8>, ver: &usize) {
     println!("getting get_rsa_patch()");
-    let find: usize =
+    let find: usize = match *ver {
         // iOS 9.x and later
-        if ver >= &2817 {
-            memmem::find(&buf, b"\x08\x69\x88\x72").unwrap_or_else(|| 
-                panic!("[-] Failed to find MOVK W8, #0x4348")
-            )
-        }
+        2817.. => memmem::find(&buf, b"\x08\x69\x88\x72").expect("[-] Failed to find MOVK W8, #0x4348"),
 
         // iOS 8.x
-        else if ver >= &2261 {
-            memmem::find(&buf, b"\x0A\x69\x88\x72").unwrap_or_else(|| 
-                panic!("[-] Failed to find MOVK W10, #0x4348")
-            )
-        }
+        2261.. => memmem::find(&buf, b"\x0A\x69\x88\x72").expect("[-] Failed to find MOVK W10, #0x4348"),
 
         // iOS 7.x
-        else if ver == &1940 {
-            memmem::find(&buf, b"\x0B\x69\x88\x72").unwrap_or_else(|| 
-                panic!("[-] Failed to find MOVK W11, #0x4348")
-            )
-        }
+        1940 => memmem::find(&buf, b"\x0B\x69\x88\x72").expect("[-] Failed to find MOVK W11, #0x4348"),
 
         //anything other version
-        else {
-            panic!("Version not supported");
-        };
+        _ => panic!("Version not supported")
+    };
     let beg_func = bof64(buf, 0, find);
-    buf[beg_func..(beg_func+8)].copy_from_slice(b"\x00\x00\x80\xD2\xC0\x03\x5F\xD6");
+    buf[range_size!(beg_func, 8)].copy_from_slice(b"\x00\x00\x80\xD2\xC0\x03\x5F\xD6");
 
     println!("[+] Patched RSA signature checks");
 }
@@ -67,106 +39,101 @@ fn get_rsa_patch(buf: &mut Vec<u8>, ver: &usize) {
 fn get_debugenabled_patch(buf: &mut Vec<u8>) {
     println!("getting get_debugenabled_patch()");
 
-    let find = memmem::find(&buf, b"debug-enabled").unwrap_or_else(|| 
-        panic!("[-] Failed to find debug-enabled string")
-    );
+    let find = memmem::find(&buf, b"debug-enabled").expect("[-] Failed to find debug-enabled string");
 
     let beg_func = xref64(buf, 0, buf.len(), find) + 0x28;
-    buf[beg_func..(beg_func+4)].copy_from_slice(b"\x20\x00\x80\xD2");
+    buf[range_size!(beg_func, 4)].copy_from_slice(b"\x20\x00\x80\xD2");
 
     println!("[+] Enabled kernel debug");
 }
 
 fn get_bootargs_patch(buf: &mut Vec<u8>, args: &String) {
-    println!("getting get_bootargs_patch(\"{}\")", *args);
+    println!("getting get_bootargs_patch(\"{}\")", args);
 
-    let find = memmem::find(&buf, b"rd=md0 nand-enable-reformat=1").unwrap_or_else(|| 
-        panic!("[-] Failed to find debug-enabled string")
-    );
+    let find = memmem::find(&buf, b"rd=md0 nand-enable-reformat=1").expect("[-] Failed to find debug-enabled string");
     
-    let arg = if args.len() < 29 {
-        println!("[*] Boot-args length {} is smaller than required, extending", args.len());
-        let needed = 29 - args.len();
-        args.to_owned() + &" ".repeat(needed)  
-    } else if args.len() > 29 {
-        println!("[*] Boot-args length {} is bigger than max length, trimming", args.len());
-        args[..29].to_string()
-    } else {
-        args.to_owned()
+    let arg = match args.len() {
+        x @ 0..=28 => {
+            println!("[*] Boot-args length {} is smaller than required, extending", x);
+            args.to_owned() + &" ".repeat(29 - x)
+        }, 
+        29 => args.to_owned(),
+        x @ 30.. => {
+            println!("[*] Boot-args length {} is bigger than max length, trimming", x);
+            args[..29].to_string()
+        },
+        val @ _ => panic!("Issue with program, value was {}", val) //should never happen, but Rust needs this wildcard anyways
     };
 
-    assert_eq!(arg.len(), 29);
-    buf[find..(find+29)].copy_from_slice(arg.as_bytes());
+    buf[range_size!(find, 29)].copy_from_slice(arg.as_bytes());
 
     println!("[+] Set xnu boot-args to \"{}\"", arg);
 }
 
-fn get_secrom_patch(buf: &mut Vec<u8>, ver: &usize) {
-    if ver == &2817 { panic!("iOS 9 iBoots aren't supported by SecureROM patch"); }
+fn get_secrom_patch(buf: &mut Vec<u8>, ver: usize) {
+    if ver == 2817 { panic!("iOS 9 iBoots aren't supported by SecureROM patch") }
     println!("getting get_secrom_patch()");
 
     /* ARM64 ASM:
-        BL tramp_init
-        MOV X1, X0
-        MOV W0, #7 @ BOOT_TARGET = BOOT_SECUREROM
-        MOV X2, #0x100000000
-        MOV X3, #0
-        BL prepare_and_jump
-    */
-
-    let prepare_and_jump;
-    let tramp_init;
+     *  BL tramp_init
+     *  MOV X1, X0
+     *  MOV W0, #7 @ BOOT_TARGET = BOOT_SECUREROM
+     *  MOV X2, #0x100000000
+     *  MOV X3, #0
+     *  BL prepare_and_jump
+     */
 
     //find prepare_and_jump()
-    let find1 = memmem::find(&buf, b"jumping into image at").unwrap_or_else(|| 
-        panic!("[-] Failed to find prepare_and_jump")
-    );
+    let find1 = memmem::find(&buf, b"jumping into image at").expect("[-] Failed to find prepare_and_jump");
 
     let beg_func1 = xref64(buf, 0, buf.len(), find1);
     
-    if ver == &1940 {
-        prepare_and_jump = follow_call64(buf, beg_func1 + 0x1c);
-        tramp_init = follow_call64(buf, beg_func1 + 0x8);
-    } else if ver == &2261 {
-        prepare_and_jump = follow_call64(buf, beg_func1 + 0x28);
-        tramp_init = follow_call64(buf, beg_func1 + 0x10);
-    } else {
-        panic!("Version not supported")
+    let prepare_and_jump;
+    let tramp_init;
+
+    match ver {
+        1940 => {
+            prepare_and_jump = follow_call64(buf, beg_func1 + 0x1c);
+            tramp_init       = follow_call64(buf, beg_func1 + 0x08);
+        },
+        2261 => {
+            prepare_and_jump = follow_call64(buf, beg_func1 + 0x28);
+            tramp_init       = follow_call64(buf, beg_func1 + 0x10);
+        },
+        _ => panic!("Version not supported")
     }
 
     //find go cmd
-    let find2 = memmem::find(&buf, b"cebilefctmbrtlhptreprmmh").unwrap_or_else(|| 
-        panic!("[-] Failed to find go cmd")
-    );
+    let find2 = memmem::find(&buf, b"cebilefctmbrtlhptreprmmh").expect("[-] Failed to find go cmd");
 
-    let beg_func2: usize;
-
-    if ver == &1940 {
-        beg_func2 = xref64(buf, 0, buf.len(), find2) - 0x44;
-    } else if ver == &2261 {
-        beg_func2 = xref64(buf, 0, buf.len(), find2) - 0x30; 
-    } else {
-        panic!("Version not supported")
-    }
+    let beg_func2 = xref64(buf, 0, buf.len(), find2) - match ver {
+        1940 => 0x44,
+        2261 => 0x30,
+        val @ _ => panic!("Issue with program, value was {}", val) //should never occur since the last match should've already panicked
+    };
     
     // write the payload
-    buf[beg_func2..(beg_func2+4)].copy_from_slice(&make_bl(beg_func2, tramp_init).to_le_bytes()); // BL tramp_init
+
+    // BL tramp_init
+    buf[range_size!(beg_func2, 4)].copy_from_slice(&make_bl(beg_func2, tramp_init)); 
+    
     /* Little Endian bytes of ARM64 ASM:
-        MOV X1, X0
-        MOV W0, #7
-        MOV X2, #100000000
-        MOV X3, #0
-    */
-    buf[(beg_func2+4)..(beg_func2+20)].copy_from_slice(b"\xE1\x03\x00\xAA\xE0\x00\x80\x52\x22\x00\xC0\xD2\x03\x00\x80\xD2");
-    buf[(beg_func2+20)..(beg_func2+24)].copy_from_slice(&make_bl(beg_func2 + 0x14, prepare_and_jump).to_le_bytes()); // BL prepare_and_jump
+     *  MOV X1, X0
+     *  MOV W0, #0x7
+     *  MOV X2, #0x100000000
+     *  MOV X3, #0x0
+     */
+    buf[range_size!(beg_func2+4, 16)].copy_from_slice(b"\xE1\x03\x00\xAA\xE0\x00\x80\x52\x22\x00\xC0\xD2\x03\x00\x80\xD2");
+    // BL prepare_and_jump
+    buf[range_size!(beg_func2+20, 4)].copy_from_slice(&make_bl(beg_func2 + 0x14, prepare_and_jump)); 
     println!("[+] Applied patch to boot SecureROM");
 }
 
 fn usage(argv: &str) {
-    println!("iPatcher-rs - tool to patch lower versions of iBoot64 in rust by @plzdonthaxme");
-    println!("Usage: {} iBoot iBoot.pwn [options]", argv);
-    println!("       -b set custom boot-args");
-    println!("       -s patch to boot SecureROM");
+    println!("iPatcher-rs - tool to patch lower versions of iBoot64 in rust by @plzdonthaxme\n\
+             Usage: {} iBoot iBoot.pwn [options]\n\
+                    -b set custom boot-args\n\
+                    -s patch to boot SecureROM", argv);
     std::process::exit(0);
 }
 
@@ -181,7 +148,7 @@ fn main() {
 
     println!("main: Starting...");
 
-    let filein = &argv[1];
+    let filein  = &argv[1];
     let fileout = &argv[2];
 
     let mut filevec: Vec<u8> = fs::read(filein).expect("[-] Failed to open iBoot, err"); //will append error message after err with colon
@@ -192,14 +159,18 @@ fn main() {
 
     if !ibss {
         get_debugenabled_patch(&mut filevec);
-        for i in 0..(argv.len()-1) {
-            match &argv[i][..] {
-                "-b" => get_bootargs_patch(&mut filevec, &argv[i+1]),
-                "-s" => get_secrom_patch(&mut filevec, &ibootver),
-                _ => (), //TODO: find invalid arguments
-            } //end match
-        } //endfor
-    } //endif
+        let mut i = 1;
+        while i < argc {
+            match argv[i].as_str() {
+                "-b" => {
+                    get_bootargs_patch(&mut filevec, &argv[i+1]);
+                    i += 1;
+                },
+                "-s" => get_secrom_patch(&mut filevec, ibootver),
+                _ => panic!("Invalid argument: {}", argv[i]) //TODO: find invalid arguments
+            }
+        }
+    }
 
     println!("[*] Writing out patched file to {}", fileout);
     fs::write(fileout, filevec).expect("[-] Failed to write iBoot to file, err");
--- src/patchfinder.rs
+++ src/patchfinder.rs
@@ -1,5 +1,6 @@
 use std::convert::TryInto;
 
+//make a u32 from bytes, and use it as a usize
 #[macro_export]
 macro_rules! u32cast {
     ($buf:expr, $var:expr) => ({
@@ -7,27 +8,48 @@ macro_rules! u32cast {
     });
 }
 
-pub fn bof64(buf: &Vec<u8>, start: usize, loc: usize) -> usize {
-    let mut _where = loc;
-    for _ in buf[start..loc].chunks(4) {
-        _where -= 4;
-        let op: usize = u32cast!(buf, _where);
-        if (op & 0xFFC003FF) == 0x910003FD {
+//create a range from the start and size
+#[macro_export]
+macro_rules! range_size {
+    ($start: expr, $size:expr) => {
+        $start..$start+$size
+    }
+}
+
+macro_rules! range {
+    ($start: expr, $end: expr, $step: expr) => {
+        ($start..$end).step_by($step)
+    }
+}
+
+pub fn bof64(buf: &Vec<u8>, start: usize, mut loc: usize) -> usize {
+    while loc >= start {
+        loc -= 4;
+        let op = u32cast!(buf, loc);
+        if op & 0xFFC003FF == 0x910003FD {
             let delta = (op >> 10) & 0xFFF;
-            //print!("%x: ADD X29, SP, #0x%x\n", where, delta);
+            //println!("%x: ADD X29, SP, #0x%x\n", where, delta);
             if (delta & 0xF) == 0 {
-                let prev = _where - (((delta >> 4) + 1) * 4) as usize;
+                let prev = loc - (((delta >> 4) + 1) * 4) as usize;
                 let mut au = u32cast!(buf, prev);
-                if (au & 0xFFC003E0) == 0xA98003E0 { /* print!("%x: STP x, y, [SP,#-imm]!\n", prev); */ return prev; } 
+                if au & 0xFFC003E0 == 0xA98003E0 { 
+                    /* print!("%x: STP x, y, [SP,#-imm]!\n", prev); */ 
+                    return prev
+                } 
                 // try something else
-                while _where > start {
-                    _where -= 4;
-                    au = u32cast!(buf, _where);
+                while loc > start {
+                    loc -= 4;
+                    au = u32cast!(buf, loc);
                     // SUB SP, SP, #imm
-                    if (au & 0xFFC003FF) == 0xD10003FF && ((au >> 10) & 0xFFF) == delta + 0x10 { return _where; }
+                    if (au & 0xFFC003FF) == 0xD10003FF && ((au >> 10) & 0xFFF) == delta + 0x10 { 
+                        return loc
+                    }
                     // STP x, y, [SP,#imm]
-                    if (au & 0xFFC003E0) != 0xA90003E0 { _where += 4; break; }
-                } //endwhile
+                    if (au & 0xFFC003E0) != 0xA90003E0 { 
+                        loc += 4;
+                        break
+                    }
+                } //end while
             } //endif
         } //endif
     } panic!("Did not find offset"); //endfor
@@ -35,21 +57,21 @@ pub fn bof64(buf: &Vec<u8>, start: usize, loc: usize) -> usize {
 
 pub fn xref64(buf: &Vec<u8>, start: usize, end: usize, what: usize) -> usize {
     let mut value: [usize; 32] = [0; 32];
-    for i in ((start & !3)..(end & !3)).step_by(4) {
+    for i in range!(start & !3, end & !3, 4) {
         let op = u32cast!(buf, i);
         let reg = op & 0x1F;
-        if (op & 0x9F000000) == 0x90000000 {
+        if op & 0x9F000000 == 0x90000000 {
             let adr = ((op & 0x60000000) >> 18) | ((op & 0xFFFFE0) << 8);
             //print!("%llx: ADRP X%d, 0x%llx\n", i, reg, ((long long)adr << 1) + (i & ~0xFFF));
             value[reg] = (adr << 1) + (i & !0xFFF);
             continue;				// XXX should not XREF on its own?
-        /*} else if (op & 0xFFE0FFE0) == 0xAA0003E0 {
-            unsigned rd = op & 0x1F;
-            unsigned rm = (op >> 16) & 0x1F;
-            //print!("%llx: MOV X%d, X%d\n", i, rd, rm);
+        /*} else if op & 0xFFE0FFE0 == 0xAA0003E0 {
+            let rd: usize = op & 0x1F;
+            let rm: usize = (op >> 16) & 0x1F;
+            //println!("%llx: MOV X%d, X%d", i, rd, rm);
             value[rd] = value[rm];
         */
-        } else if (op & 0xFF000000) == 0x91000000 {
+        } else if op & 0xFF000000 == 0x91000000 {
             let rn = (op >> 5) & 0x1F;
             let shift = (op >> 22) & 0x3;
             let mut imm = (op >> 10) & 0xFFF;
@@ -57,43 +79,44 @@ pub fn xref64(buf: &Vec<u8>, start: usize, end: usize, what: usize) -> usize {
                 imm <<= 12; 
             } else {
                 //assert_eq!(shift, 0); 
-                if shift > 1 {continue} 
+                if shift > 1 { continue } 
             }
-            //print!("%llx: ADD X%d, X%d, 0x%x\n", i, reg, rn, imm);
+            //println!("%llx: ADD X%d, X%d, 0x%x", i, reg, rn, imm);
             value[reg] = value[rn] + imm;
-        } else if (op & 0xF9C00000) == 0xF9400000 {
+        } else if op & 0xF9C00000 == 0xF9400000 {
             let rn = (op >> 5) & 0x1F;
             let imm = ((op >> 10) & 0xFFF) << 3;
-            //print!("%llx: LDR X%d, [X%d, 0x%x]\n", i, reg, rn, imm);
-            if imm == 0 {continue}			// XXX not counted as true xref
+            //println!("%llx: LDR X%d, [X%d, 0x%x]", i, reg, rn, imm);
+            if imm == 0 { continue }			// XXX not counted as true xref
             value[reg] = value[rn] + imm;	// XXX address, not actual value
         /*} else if (op & 0xF9C00000) == 0xF9000000 {
-            let rn: u32 = (op >> 5) & 0x1F;
-            let imm: u32 = ((op >> 10) & 0xFFF) << 3;
-            //print!("%llx: STR X%d, [X%d, 0x%x]\n", i, reg, rn, imm);
+            let rn: usize = (op >> 5) & 0x1F;
+            let imm: usize = ((op >> 10) & 0xFFF) << 3;
+            //println!("%llx: STR X%d, [X%d, 0x%x]", i, reg, rn, imm);
             if imm == 0 { continue };			// XXX not counted as true xref
             value[rn as usize] = value[rn as usize] + (imm as u64);	// XXX address, not actual value
         */
-        } else if (op & 0x9F000000) == 0x10000000 {
+        } else if op & 0x9F000000 == 0x10000000 {
             let adr = ((op & 0x60000000) >> 18) | ((op & 0xFFFFE0) << 8);
-            //print!("%llx: ADR X%d, 0x%llx\n", i, reg, ((long long)adr >> 11) + i);
+            //println!("%llx: ADR X%d, 0x%llx", i, reg, ((long long)adr >> 11) + i);
             value[reg] = (adr >> 11) + i;
-        } else if (op & 0xFF000000) == 0x58000000 {
+        } else if op & 0xFF000000 == 0x58000000 {
             let adr = (op & 0xFFFFE0) >> 3;
-            //print!("%llx: LDR X%d, =0x%llx\n", i, reg, adr + i);
+            //println!("%llx: LDR X%d, =0x%llx", i, reg, adr + i);
             value[reg] = adr + i;		// XXX address, not actual value
         } //endif
 
-        if value[reg] == what { return i; }
-    } panic!("Did not find offset"); //endfor
+        if value[reg] == what { return i }
+    } //end for
+    panic!("Did not find offset")
 }
 
-pub fn make_bl(from: usize, to: usize) -> u32 {
-    if from > to { 
-        (0x18000000 - (from - to) / 4) as u32
+pub fn make_bl(from: usize, to: usize) -> [u8; 8] {
+    if from > to {
+        0x18000000 - (from - to) / 4
     } else {
-        (0x94000000 + (to - from) / 4) as u32
-    }
+        0x94000000 + (to - from) / 4
+    }.to_le_bytes()
 }
 
 pub fn follow_call64(buf: &Vec<u8>, call: usize) -> usize {
